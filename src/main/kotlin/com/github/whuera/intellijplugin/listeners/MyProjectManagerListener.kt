334533e01e7ccc3152f8749ea51f750bad1d6316
GitHub Action <action@github.com>
2020-10-29 20:44:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/whuera/intellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/whuera/intellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.whuera.intellijplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.whuera.intellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/whuera/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/whuera/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.whuera.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.whuera.intellijplugin.MyBundle
 
 class MyApplicationService {
 
